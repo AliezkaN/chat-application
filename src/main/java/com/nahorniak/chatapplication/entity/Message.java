af747b3b5e160731a4551ebf0bddc4a0320134cf
“Oleh <nagornyak68@gmail.com>
2023-01-15 12:04:26 +0200

    add datetime to Message

--- src/main/java/com/nahorniak/chatapplication/entity/Message.java
+++ src/main/java/com/nahorniak/chatapplication/entity/Message.java
@@ -3,16 +3,26 @@ package com.nahorniak.chatapplication.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 @Data
 @NoArgsConstructor
 public class Message {
     private String sender;
     private String content;
     private MessageType type;
+    private LocalDateTime dateTime;
 
     public enum MessageType{
         ALIVE,
         JOINED,
         LEFT
+    }
+
+    public void setDateTime(Long milliseconds) {
+        this.dateTime = Instant.ofEpochMilli(milliseconds).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 }
